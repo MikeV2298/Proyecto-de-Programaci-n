d2dcf68d8cee076021d5ddf63a0e08e485d60f26
MikeV2298 <changoburgosma@gmail.com>
2022-03-02 10:08:19 -0500

    ejercicio 1 de while

---
+++ while.py
@@ -0,0 +1,11 @@
+#10-20
+
+num=-1
+while num <10 or num>20 or num%2!=0:
+    num=int(input("Ingrese nuevamente un numero:"))
+print("....")
+
+#Otra forma de resolverlo
+#while not(num>=10 and num <=20 and num%2==0):
+#    num=int(input("Ingrese nuevamente un numero:"))
+#print("....")
